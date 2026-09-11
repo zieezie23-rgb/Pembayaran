7218c2f3400dfd0c37ca21509712f42d6499be64
zie <zieezie23@gmail.com>
2026-09-11 13:45:13 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="k2-pay-v10";
+const ASSETS=["./","./index.html","./manifest.json","./assets/icon.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
